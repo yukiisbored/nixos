525e65b6bb824878cd750a1cbdc3a885b363b9eb
Muhammad Kaisar Arkhan (Yuki) <hi@yukiisbo.red>
2021-07-16 06:20:13 +0200

    Flake-ify

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "Yuki's NixOS configuration";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.astolfo = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
