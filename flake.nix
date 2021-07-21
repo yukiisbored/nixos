ea4b3142232d70d09dc3351b43102dff2149b6d3
Muhammad Kaisar Arkhan (Yuki) <hi@yukiisbo.red>
2021-07-21 20:19:00 +0200

    Add impermanence

--- flake.nix
+++ flake.nix
@@ -1,12 +1,16 @@
 {
   description = "Yuki's configuration for Nix systems";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    impermanence.url = "github:nix-community/impermanence";
+  };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, impermanence }: {
     nixosConfigurations.astolfo = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        impermanence.nixosModules.impermanence
         ./modules/core.nix
         ./modules/gayming.nix
         ./secrets/astolfo.nix
