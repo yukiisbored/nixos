bc227f317f213331ae9239837940b17f557e9768
Yuki Langley <hi@yukiisbo.red>
2022-10-25 11:59:50 +0200

    Add touchegg

--- modules/core.nix
+++ modules/core.nix
@@ -60,7 +60,10 @@
     libinput.enable = true;
   };
 
-  services.trezord.enable = true;
+  services = {
+    trezord.enable = true;
+    touchegg.enable = true;
+  };
 
   environment = {
     systemPackages = with pkgs; [
